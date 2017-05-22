8f65cda79cfa21171b90b5f296fcc80c41562dd3
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2017-05-23 01:33:57 +0200

    Added Kubernetes discovery example.

---
+++ Discovery/Kubernetes/client/simple-k8s-client.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+var url string
+
+func main() {
+	initServiceURL()
+
+	var client = &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	callHelloEvery(5*time.Second, client)
+}
+
+func initServiceURL() {
+	url = os.Getenv("SERVICE_URL")
+	if len(url) == 0 {
+		url = "http://simple-k8s-server:9090/info"
+	}
+}
+
+func hello(t time.Time, client *http.Client) {
+	// Call the greeter
+	response, err := client.Get(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// print response
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Printf("%s. Time is %v\n", body, t)
+}
+
+func callHelloEvery(d time.Duration, client *http.Client) {
+	for x := range time.Tick(d) {
+		hello(x, client)
+	}
+}
---
+++ Discovery/Kubernetes/server/simple-k8s-server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/info", info)
+	http.ListenAndServe(port(), nil)
+}
+
+func info(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("The /info endpoint is being called...")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello Kubernetes Discovery")
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "9090"
+	}
+	return ":" + port
+}
