76c13ad714d16774c938cca79b710b4152498b31
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2017-05-23 00:56:13 +0200

    Added Consul discovery for simple client and server.

---
+++ Discovery/Simple/client/simple-client.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	consulapi "github.com/hashicorp/consul/api"
+)
+
+var url string
+
+func main() {
+	lookupServiceWithConsul()
+
+	var client = &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	callHelloEvery(5*time.Second, client)
+}
+
+func lookupServiceWithConsul() {
+	config := consulapi.DefaultConfig()
+	consul, error := consulapi.NewClient(config)
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	services, error := consul.Agent().Services()
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	address := services["simple-server"].Address
+	port := services["simple-server"].Port
+
+	url = fmt.Sprintf("http://%s:%v/info", address, port)
+}
+
+func hello(t time.Time, client *http.Client) {
+	// Call the greeter
+	response, err := client.Get(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// print response
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Printf("%s. Time is %v\n", body, t)
+}
+
+func callHelloEvery(d time.Duration, client *http.Client) {
+	for x := range time.Tick(d) {
+		hello(x, client)
+	}
+}
---
+++ Discovery/Simple/server/simple-server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	consulapi "github.com/hashicorp/consul/api"
+)
+
+func main() {
+	registerServiceWithConsul()
+
+	http.HandleFunc("/info", info)
+	http.ListenAndServe(port(), nil)
+}
+
+func registerServiceWithConsul() {
+	config := consulapi.DefaultConfig()
+	consul, err := consulapi.NewClient(config)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var registration = new(consulapi.AgentServiceRegistration)
+
+	registration.ID = "simple-server"
+	registration.Name = "simple-server"
+
+	address := hostname()
+	registration.Address = address
+	port, _ := strconv.Atoi(port()[1:len(port())])
+	registration.Port = port
+
+	registration.Check = new(consulapi.AgentServiceCheck)
+	registration.Check.HTTP = fmt.Sprintf("http://%s:%v/info", address, port)
+	registration.Check.Interval = "5s"
+	registration.Check.Timeout = "3s"
+
+	consul.Agent().ServiceRegister(registration)
+}
+
+func info(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("The /info endpoint is being called...")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello Consul Discovery")
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
+}
+
+func hostname() string {
+	hostname, _ := os.Hostname()
+	return hostname
+}
